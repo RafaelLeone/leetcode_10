7d610ead89c541290cda469bd12e19d1edca1622
RafaelLeone <rinaciodacosta@gmail.com>
2023-01-30 15:43:30 -0300

    corrigido 28

--- 28_find_the_index_of_the_first_occurrence_in_a_string.py
+++ 28_find_the_index_of_the_first_occurrence_in_a_string.py
@@ -1,30 +1,29 @@
 # https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
 
 # Input: 
-palheiro = "tristemastriste"
-agulha = "triste"
+haystack = "tristemastriste"
+needle = "triste"
 # Output: 0
 # Explicação: "triste" aparece nos índices 0 e 6.
 # A primeira ocorrência é no índice 0, então o retorno é 0.
 # Se a agulha não estiver no palheiro, retorna-se -1.
 
-TAMANHO_AGULHA = len(agulha)
-teve_resultado = False
+#Copiar a partir daqui dentro do Class Solution no site em Python3:
+def strStr(self, haystack: str, needle: str) -> int:
+        TAMANHO_AGULHA = len(needle)
+        for posicao, caracter in enumerate(haystack):
+            contador_de_posicoes_iguais = 0
+            try:
+                for i in range(posicao, posicao+TAMANHO_AGULHA):
+                    if haystack[i] != needle[i-posicao]:
+                        continue
+                    else:
+                        contador_de_posicoes_iguais += 1
+                if contador_de_posicoes_iguais == TAMANHO_AGULHA:
+                    return posicao
+            except:
+                break
 
-for posicao, caracter in enumerate(palheiro):
-    contador_de_posicoes_iguais = 0
-    try:
-        for i in range(posicao, posicao+TAMANHO_AGULHA):
-            if palheiro[i] != agulha[i-posicao]:
-                continue
-            else:
-                contador_de_posicoes_iguais += 1
-        if contador_de_posicoes_iguais == TAMANHO_AGULHA:
-            print(posicao)
-            teve_resultado = True
-            exit()
-    except:
-        break
-
-if not teve_resultado:
-    print(-1)
+        return -1
+#Não copiar para o site:
+print(strStr(..., haystack, needle))
