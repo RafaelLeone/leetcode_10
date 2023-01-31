677bb6b440491d03e26118fe9aed0eab631e863a
RafaelLeone <rinaciodacosta@gmail.com>
2023-01-31 16:03:41 -0300

    hard1 corrigido

--- 04-median-of-two-sorted-arrays.js
+++ 04-median-of-two-sorted-arrays.js
@@ -1,3 +1,12 @@
+// https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+//Input: 
+let nums1 = [1,3]
+let nums2 = [2]
+// Output: 2.00000
+// Explanation: merged array = [1,2,3] and median is 2.
+
+// Copiar a partir daqui para o site em JavaScript:
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
@@ -7,7 +16,7 @@ var findMedianSortedArrays = function(nums1, nums2) {
     for (let numero of nums2) {
         nums1.push(numero)
     }
-    nums1.sort((a, b) => {
+    nums1.sort((a, b) => { //para os casos negativos, o js não ordena sozinho.
         if (a < b) {
           return -1;
         }
@@ -23,7 +32,5 @@ var findMedianSortedArrays = function(nums1, nums2) {
         return ((nums1[(nums1.length/2)-1])+(nums1[(nums1.length/2)]))/2
     }
 };
-
-let nums1 = [3]
-let nums2 = [-2, -1]
+// Não copiar no site:
 console.log(findMedianSortedArrays(nums1, nums2))
